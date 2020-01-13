908ff2e85d2f5ce21a45b63a4b359a0d7e404505
olga.shafran <olga.shafran@ibm.com>
2020-01-13 14:04:34 -0500

    add demo messages

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 @app.route('/setredis/', methods=['POST'])
 # http://127.0.0.1:5000/setredis?key=keyfrompython1&value=vlauefrompostman
 def setredis():
-    key = request.args.get('key')
-    value = request.args.get('value')
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.set(key, value)
+    # key = request.args.get('key')
+    # value = request.args.get('value')
+    # r = redis.Redis(host='localhost', port=6379, db=0)
+    # r.set(key, value)
     return "set done!"
 
 
@@ -23,26 +23,28 @@ def setredis():
 # http://127.0.0.1:5000/getredis/keyfrompython
 # def getredis(key):
 def getredis():
-    key = request.args.get('key')
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    try:
-        value = r.get(key)
-        if value:
-            return value
-        else:
-            return 'no data for this key'
-    except:
-        return None
+    return "getredis demo"
+    # key = request.args.get('key')
+    # r = redis.Redis(host='localhost', port=6379, db=0)
+    # try:
+    #     value = r.get(key)
+    #     if value:
+    #         return value
+    #     else:
+    #         return 'no data for this key'
+    # except:
+    #     return None
 
 @app.route('/getallkeys', methods=['GET'])
 # http://127.0.0.1:5000/getallkeys
 def getallkeys():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    retlist = [];
-    for key in r.scan_iter():
-        retlist.append(key.decode("utf-8"))
-    print(retlist)
-    return json.dumps(retlist)
+    return "getallkeys demo"
+    # r = redis.Redis(host='localhost', port=6379, db=0)
+    # retlist = [];
+    # for key in r.scan_iter():
+    #     retlist.append(key.decode("utf-8"))
+    # print(retlist)
+    # return json.dumps(retlist)
 
 
 
