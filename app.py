6452205e11f99df40472d474f07234010e1a9278
olga.shafran <olga.shafran@ibm.com>
2020-01-15 13:01:51 -0500

    add params

--- app.py
+++ app.py
@@ -1,37 +1,64 @@
+#!/usr/bin/python
+
 import socket
-import sys
+import sys, getopt
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to the port
-server_address = ('localhost', 5001)
-my_str = 'starting up on %s port %s' % server_address
-print(my_str)
-
-print('starting up on %s port %s' % server_address)
-sock.bind(server_address)
-# Listen for incoming connections
-sock.listen(1)
-
-while True:
-    # Wait for a connection
-    print ('waiting for a connection')
-    connection, client_address = sock.accept()
-
+def main(argv):
+    print ('start')
+    ip_addr = 'localhost'
+    ip_port = 5001
     try:
-        print ('connection from', client_address)
+        opts, args = getopt.getopt(argv, "hi:p:", ["ip_addr=", "ip_port="])
+    except getopt.GetoptError:
+        print('app.py -i <ip_addr> -p <ip_port>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('app.py -i <ip_addr> -p <ip_port>')
+            sys.exit()
+        elif opt in ("-i", "--ip_addr"):
+            ip_addr = arg
+        elif opt in ("-p", "--ip_port"):
+            ip_port = arg
+    print('ip_addr is "', ip_addr)
+    print('ip_port"', ip_port)
+    print('done')
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(160)
-            print ('received "%s"' % data)
-            if data:
-                print ('sending data back to the client')
-                connection.sendall(data)
-            else:
-                print ('no more data from', client_address)
-                break
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the port
+    server_address = (ip_addr, int(ip_port))
+    my_str = 'starting up on %s port %s' % server_address
+    print(my_str)
 
-    finally:
-        # Clean up the connection
-        connection.close()
+    print('starting up on %s port %s' % server_address)
+    sock.bind(server_address)
+    # Listen for incoming connections
+    sock.listen(1)
+
+    while True:
+        # Wait for a connection
+        print ('waiting for a connection')
+        connection, client_address = sock.accept()
+
+        try:
+            print ('connection from', client_address)
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(160)
+                print ('received "%s"' % data)
+                if data:
+                    print ('sending data back to the client')
+                    connection.sendall(data)
+                else:
+                    print ('no more data from', client_address)
+                    break
+
+        finally:
+            # Clean up the connection
+            connection.close()
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- app_client.py
+++ app_client.py
@@ -1,30 +1,61 @@
 import socket
-import sys
+import sys, getopt
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main(argv):
+    print('start')
+    ip_addr = 'localhost'
+    ip_port = 5001
+    message = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:p:m:", ["ip_addr=", "ip_port=", "message="])
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 5001)
-print ('connecting to %s port %s' % server_address)
-sock.connect(server_address)
+    except getopt.GetoptError:
+        print('app.py -i <ip_addr> -p <ip_port>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('app.py -i <ip_addr> -p <ip_port>')
+            sys.exit()
+        elif opt in ("-i", "--ip_addr"):
+            ip_addr = arg
+        elif opt in ("-p", "--ip_port"):
+            ip_port = arg
+        elif opt in ("-m", "--message"):
+            message = arg
+    print('ip_addr is "', ip_addr)
+    print('ip_port"', ip_port)
+    print('message"', message)
+    print('done')
 
-try:
 
-    # Send data
-    message = 'This is the message.  It will be repeated.'
-    print ('sending "%s"' % message)
-    sock.sendall(str.encode(message))
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
+    # Connect the socket to the port where the server is listening
+    server_address = (ip_addr, int(ip_port))
+    print ('connecting to %s port %s' % server_address)
+    sock.connect(server_address)
 
-    while amount_received < amount_expected:
-        data = sock.recv(160)
-        amount_received += len(data)
-        print ('received "%s"' % data)
+    try:
 
-finally:
-    print ('closing socket')
-    sock.close()
+        # Send data
+        # message = 'This is the message.  It will be repeated.'
+        print ('sending "%s"' % message)
+        sock.sendall(str.encode(message))
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+
+        while amount_received < amount_expected:
+            data = sock.recv(160)
+            amount_received += len(data)
+            print ('received "%s"' % data)
+
+    finally:
+        print ('closing socket')
+        sock.close()
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
